25cb6859695c5e369bb4301ce0586d5c5710546c
Juan <jlizarraga@unomaha.edu>
2023-06-02 13:58:37 -0500

    adding logic for displaying "Visit Website" button.

--- Home/views.py
+++ Home/views.py
@@ -6,15 +6,23 @@ def our_work(request):
     # get a collection of all, projects.
     projects = Project.objects.all()
 
+
+
     return render(request, "our_work.html", {'projects': projects})
 
 def project_details(request, pk):
     # get the project, based off of the pk that we passed in.
     project = Project.objects.filter(pk=pk).get()
 
+    # if the project has a link, associated with, it pass bool variable to template to create a button underneath image.
+    if project.link != "":
+        link = True
+    else:
+        link = False
+
     # query all of the images belonging to this proejct.
     images = ProjectGallery.objects.filter(project_id=pk)
-    return render(request, "project_details.html", {'project': project, 'images': images})
+    return render(request, "project_details.html", {'project': project, 'images': images, 'link': link})
 
 def home(request):
     return render(request, "index.html")
